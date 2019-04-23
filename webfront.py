ed0ae0dd1bea990b256e9beb057246306577b374
Brian Copeland <brian@copeland.bz>
2019-04-23 12:22:50 -0500

    Matching prod to current commit

--- webfront.py
+++ webfront.py
@@ -3,12 +3,28 @@ from threading import Lock
 from werkzeug.serving import run_simple
 from datetime import datetime
 
+from pywebpush import webpush, WebPushException
+
 import sqlite3
 
 app = Flask(__name__)
 
 thread = None
 thread_lock = Lock()
+
+#### Public / private keys for push notifications
+publicKey = 'BFU1O8aegR7BDQ2HQ5XaCe0L8itsFL6XJyuz7goH1Dj9Y6todufGqL8_LYlqkAUGaTLM7VpK9XDE4t-3IDDet0I'
+
+with open('push_key.pem') as f:
+    privateKey = f.read()
+# Read adds a newline at the end, lets strip that out
+privateKey = privateKey.strip('\n')
+
+### Secret Carl Url Readin
+with open('secret_carl_url.pem') as f:
+    secret_url = f.read()
+secret_url = secret_url.strip('\n')
+
 
 def dict_factory(cursor, row):
     d = {}
@@ -38,6 +54,33 @@ def average_temp(temp_score):
     else:
         return "broken"
 
+def send_push(new_temp):
+    with sqlite3.connect('watertemp.db') as conn:
+        cget = conn.cursor()
+        cget.row_factory = dict_factory
+        cget.execute("""SELECT endpoint, p256dh, auth FROM subscriptions""")
+        x = cget.fetchall()
+    for i in x:
+        keys = dict({"p256dh": i["p256dh"], "auth": i["auth"]})
+        subscription_info = dict({'endpoint': i["endpoint"], 'keys': keys})
+        try:
+            webpush(
+                subscription_info,
+                data="Carl's water is " + new_temp + ".",
+                vapid_private_key=privateKey,
+                vapid_claims={"sub": "mailto:brian@copeland.bz"}
+            )
+        except WebPushException as ex:
+            print("I'm sorry, Dave, but I can't do that: {}", repr(ex))
+            # Mozilla returns additional information in the body of the response.
+            if ex.response and ex.response.json():
+                extra = ex.response.json()
+                print("Remote service replied with a {}:{}, {}",
+                      extra.code,
+                      extra.errno,
+                      extra.message
+                      )
+    return("Push sent")
 
 
 @app.route('/')
@@ -73,51 +116,47 @@ def home():
         today_temp_string=today_temp_string
         )
 
-@app.route('/secret_carl_url')
+@app.route('/{}'.format(secret_url))
 def test_switch():
     return render_template('index.html')
 
-@app.route('/_new_too_cold')
-def new_too_cold():
+@app.route('/update-water')
+def update_water():
+    temp_update = request.args.get('temp')
+    if temp_update is None:
+        print("No temp parameter in update-water call.")
+        return jsonify(results = "parameter temp is required"), 400
+    elif temp_update not in ["too-cold", "cold", "perfect", "lukewarm", "hot"]:
+        print("temp value not valid")
+        return jsonify(results = "temp must be too-cold, cold, perfect, lukewarm or hot"), 400
+    else:
+        print("Updating temp to %s".format(temp_update))
+        with sqlite3.connect('watertemp.db') as conn:
+            cput = conn.cursor()
+            cput.execute("INSERT INTO watertemp (sqltime, watertemp) VALUES (CURRENT_TIMESTAMP, ?)", (temp_update,))
+            cput.close()
+        # Send push notifications
+        send_push(temp_update)
+        return jsonify(result="updated to too cold")
+
+
+@app.route('/note')
+def note():
+    return render_template('note.html')
+
+@app.route('/save-subscription', methods=['POST'])
+def add_subscription():
+    content=request.json
     with sqlite3.connect('watertemp.db') as conn:
         cput = conn.cursor()
-        cput.execute("INSERT INTO watertemp (sqltime, watertemp) VALUES (CURRENT_TIMESTAMP, ?)", ("too-cold",))
+        cput.execute("INSERT INTO subscriptions (endpoint, p256dh, auth) VALUES (?, ?, ?)", (content['endpoint'],
+        content['keys']['p256dh'],
+        content['keys']['auth'],
+        ))
         cput.close()
-    return jsonify(result="updated to too cold")
-
-@app.route('/_new_cold')
-def new_cold():
-    with sqlite3.connect('watertemp.db') as conn:
-        cput = conn.cursor()
-        cput.execute("INSERT INTO watertemp (sqltime, watertemp) VALUES (CURRENT_TIMESTAMP, ?)", ("cold",))
-        cput.close()
-    return jsonify(result="updated to cold")
-
-
-@app.route('/_new_perfect')
-def new_perfect():
-    with sqlite3.connect('watertemp.db') as conn:
-        cput = conn.cursor()
-        cput.execute("INSERT INTO watertemp (sqltime, watertemp) VALUES (CURRENT_TIMESTAMP, ?)", ("perfect",))
-        cput.close()
-    return jsonify(result="updated to perfect")
-
-
-@app.route('/_new_lukewarm')
-def new_lukewarm():
-    with sqlite3.connect('watertemp.db') as conn:
-        cput = conn.cursor()
-        cput.execute("INSERT INTO watertemp (sqltime, watertemp) VALUES (CURRENT_TIMESTAMP, ?)", ("lukewarm",))
-        cput.close()
-    return jsonify(result="updated to lukewarm")
-
-@app.route('/_new_hot')
-def new_hot():
-    with sqlite3.connect('watertemp.db') as conn:
-        cput = conn.cursor()
-        cput.execute("INSERT INTO watertemp (sqltime, watertemp) VALUES (CURRENT_TIMESTAMP, ?)", ("hot",))
-        cput.close()
-    return jsonify(result="updated to hot")
+    data = {'response': 'Subscription created'}
+    return jsonify(data), 200
+    #return 200
 
 application = app
 
